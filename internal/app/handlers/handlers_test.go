cb25827ba49cb968d0b4c10485c9adb8ff4228b9
N0rkton <shvm20031807@gmail.com>
2024-03-06 20:34:47 +0300

    починилчутка

--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -1,5 +1,17 @@
 package handlers
 
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
 // !func Test_handlerWrapper_IndexPage(t *testing.T) {
 // 	type args struct {
 // 		res http.ResponseWriter
@@ -19,47 +31,49 @@ package handlers
 // 	}
 // }
 
-// !func Test_handlerWrapper_IndexPage(t *testing.T) {
-// 	type want struct {
-// 		code        int
-// 		response    string
-// 		contentType string
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		want want
-// 	}{
-// 		{
-// 			name: "positive test #1",
-// 			want: want{
-// 				code:        201,
-// 				response:    "https://example.com",
-// 				contentType: "text/plain",
-// 			},
-// 		},
-// 	}
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			hw := Init()
-// 			reqBody := strings.NewReader("https://example.com")
-// 			request := httptest.NewRequest(http.MethodPost, "/status", reqBody)
-// 			// создаём новый Recorder
-// 			w := httptest.NewRecorder()
-// 			hw.IndexPage(w, request)
+func Test_handlerWrapper_IndexPage(t *testing.T) {
+	type want struct {
+		code int
+		//response    string
+		contentType string
+	}
+	tests := []struct {
+		name    string
+		reqBody string
+		want    want
+	}{
+		{
+			name:    "positive test #1",
+			reqBody: "http://site1.com",
+			want: want{
+				code: 201,
+				//response:    "https://example.com",
+				contentType: "text/plain",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hw := Init()
+			reqreqBody := strings.NewReader(string(test.reqBody))
+			request := httptest.NewRequest(http.MethodPost, "/", reqreqBody)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			hw.IndexPage(w, request)
 
-// 			res := w.Result()
-// 			// проверяем код ответа
-// 			assert.Equal(t, test.want.code, res.StatusCode)
-// 			// получаем и проверяем тело запроса
-// 			defer res.Body.Close()
-// 			resBody, err := io.ReadAll(res.Body)
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, test.want.code, res.StatusCode)
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resreqBody, err := io.ReadAll(res.Body)
 
-// 			require.NoError(t, err)
-// 			assert.Equal(t, test.want.response, string(resBody))
-// 			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
-// 		})
-// 	}
-// }
+			require.NoError(t, err)
+			require.NotNil(t, resreqBody)
+			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
+		})
+	}
+}
 
 // !func Test_handlerWrapper_Redirect(t *testing.T) {
 // 	type want struct {
@@ -91,11 +105,11 @@ package handlers
 // 			// проверяем код ответа
 // 			assert.Equal(t, test.want.code, res.StatusCode)
 // 			// получаем и проверяем тело запроса
-// 			defer res.Body.Close()
-// 			resBody, err := io.ReadAll(res.Body)
+// 			defer res.reqBody.Close()
+// 			resreqBody, err := io.ReadAll(res.reqBody)
 
 // 			require.NoError(t, err)
-// 			assert.Equal(t, test.want.response, string(resBody))
+// 			assert.Equal(t, test.want.response, string(resreqBody))
 
 // 		})
 // 	}
@@ -104,24 +118,24 @@ package handlers
 // !func TestIndexPage(t *testing.T) {
 // 	hw := Init()
 
-// 	reqBody := []byte("https://example.com")
-// 	req := httptest.NewRequest("POST", "/", bytes.NewBuffer(reqBody))
+// 	reqreqBody := []byte("https://example.com")
+// 	req := httptest.NewRequest("POST", "/", bytes.NewBuffer(reqreqBody))
 // 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 // 	w := httptest.NewRecorder()
 
 // 	hw.IndexPage(w, req)
 
 // 	resp := w.Result()
-// 	body := make([]byte, resp.ContentLength)
-// 	resp.Body.Read(body)
+// 	reqBody := make([]byte, resp.ContentLength)
+// 	resp.reqBody.Read(reqBody)
 
 // 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 // 	assert.Equal(t, "text/plain", resp.Header.Get("content-type"))
-// 	assert.Contains(t, string(body), "localhost")
-// 	assert.Equal(t, 31, len(string(body)))
+// 	assert.Contains(t, string(reqBody), "localhost")
+// 	assert.Equal(t, 31, len(string(reqBody)))
 
 // 	value, _ := hw.storageInterface.Get("rez1")
-// 	assert.Equal(t, string(reqBody), value)
+// 	assert.Equal(t, string(reqreqBody), value)
 // }
 
 // !func TestIndexPage(t *testing.T) {
@@ -139,47 +153,46 @@ package handlers
 // 	assert.Equal(t, http.StatusCreated, rr.Code)
 
 // 	// Проверяем длину сгенерированной строки
-// 	body := rr.Body.String()
-// 	assert.Len(t, body, 6)
+// 	reqBody := rr.reqBody.String()
+// 	assert.Len(t, reqBody, 6)
 
 // 	// Проверяем, что сгенерированная строка начинается с localhost
-// 	assert.True(t, strings.HasPrefix(body, localhost))
+// 	assert.True(t, strings.HasPrefix(reqBody, localhost))
 
 // 	// Проверяем, что сгенерированная строка добавлена в хранилище
 // 	require.NotNil(t, handler.storageInterface)
-// 	value, err := handler.storageInterface.Get(body[len(localhost):])
+// 	value, err := handler.storageInterface.Get(reqBody[len(localhost):])
 // 	require.NoError(t, err)
 // 	assert.Equal(t, "https://example.com", value)
 // }
 
-// *func TestIndexPage(t *testing.T) {
-// 	// Создаем тестовый запрос
-// 	req := httptest.NewRequest(http.MethodPost, localhost, bytes.NewReader([]byte("https://example.com")))
+func TestIndexPage(t *testing.T) {
+	// Создаем тестовый запрос
+	req := httptest.NewRequest(http.MethodPost, localhost, bytes.NewReader([]byte("https://example.com")))
 
-// 	// Создаем тестовый обработчик
-// 	handler := Init()
+	// Создаем тестовый обработчик
+	handler := Init()
 
-// 	// Выполняем запрос
-// 	rr := httptest.NewRecorder()
-// 	handler.IndexPage(rr, req)
+	// Выполняем запрос
+	rr := httptest.NewRecorder()
+	handler.IndexPage(rr, req)
 
-// 	// Проверяем код ответа
-// 	assert.Equal(t, http.StatusCreated, rr.Code)
+	// Проверяем код ответа
+	assert.Equal(t, http.StatusCreated, rr.Code)
 
-// 	// Проверяем длину сгенерированной строки
-// 	body := rr.Body.String()
-// 	assert.Len(t, body[len(localhost):], 6)
+	// Проверяем длину сгенерированной строки
+	reqBody := rr.Body.String()
+	assert.Len(t, reqBody[len(localhost):], 6)
 
-// 	// Проверяем, что сгенерированная строка начинается с localhost
-// 	assert.True(t, strings.HasPrefix(body, localhost))
+	// Проверяем, что сгенерированная строка начинается с localhost
+	assert.True(t, strings.HasPrefix(reqBody, localhost))
 
-//		// Проверяем, что сгенерированная строка добавлена в хранилище
-//		require.NotNil(t, handler.storageInterface)
-//		value, err := handler.storageInterface.Get(body[len(localhost):])
-//		require.NoError(t, err)
-//		assert.NotEmpty(t, value)
-//	}
-//
+	// Проверяем, что сгенерированная строка добавлена в хранилище
+	require.NotNil(t, handler.storageInterface)
+	value, err := handler.storageInterface.Get(reqBody[len(localhost):])
+	require.NoError(t, err)
+	assert.NotEmpty(t, value)
+}
 
 // !func TestIndexPage(t *testing.T) {
 // 	type want struct {
@@ -218,15 +231,15 @@ package handlers
 // 			assert.Equal(t, test.want.code, rr.Code)
 
 // 			// Проверяем длину сгенерированной строки
-// 			body := rr.Body.String()
-// 			assert.Len(t, body[len(localhost):], 6)
+// 			reqBody := rr.reqBody.String()
+// 			assert.Len(t, reqBody[len(localhost):], 6)
 
 // 			// Проверяем тип контента
 // 			assert.Equal(t, test.want.contentType, rr.Header().Get("Content-Type"))
 
 // 			// Проверяем, что сгенерированная строка добавлена в хранилище
 // 			require.NotNil(t, handler.storageInterface)
-// 			value, err := handler.storageInterface.Get(body[len(localhost):])
+// 			value, err := handler.storageInterface.Get(reqBody[len(localhost):])
 // 			require.NoError(t, err)
 // 			assert.NotEmpty(t, value)
 // 		})
