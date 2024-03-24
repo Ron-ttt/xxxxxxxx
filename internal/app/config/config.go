a2d22f5afd5ed490427550f93f40331670e284b0
Nika <v.mezerina@gmail.com>
2024-03-24 19:26:50 +0300

    -

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 func Flags() (string, string) {
 	// Определение флагов
@@ -8,5 +11,11 @@ func Flags() (string, string) {
 	baseURL := flag.String("b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL") // порты должны совпадать иначе кабзда
 	// Парсинг флагов
 	flag.Parse()
+	if envAddress := os.Getenv("SERVER_ADDRESS"); envAddress != "" {
+		*address = envAddress
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		*baseURL = envBaseURL
+	}
 	return *address, *baseURL
 }
